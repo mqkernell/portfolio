99cd994ac43608b9e76b7dad9d604478e280fd01
mqkernell <mqkernell@wustl.edu>
2020-02-06 11:39:50 -0600

    setting up project

---
+++ data.js
@@ -0,0 +1,23 @@
+let project = [
+    {
+      "name": "lost & found",
+      "medium": "photography",
+      "tags": [
+        "found-objects",
+        "xxx"
+      ],
+      "date": "2019",
+      "client": "",
+      "recognition": "",
+      "images": [
+        {
+          "src": "",
+          "alt": "alt title"
+        },
+        {
+          "src": "",
+          "alt": ""
+        }
+      ]
+    }
+  ]
---
+++ index.js
@@ -0,0 +1,14 @@
+
+$( document ).ready(function() {
+    main();
+});
+
+function main() {
+
+console.log("hello world!!!");
+
+let activeButtons = $(".button-active");
+let selectedButton = $(".selected");
+
+}
+
