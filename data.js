8386f2fbcb743dd2fd2a3c19a8d776a08fa8d8b9
mqkernell <mqkernell@wustl.edu>
2020-02-06 12:54:02 -0600

    Changing data

--- data.js
+++ data.js
@@ -1,17 +1,84 @@
-let project = [
+let projects = [
     {
       "name": "lost & found",
+      "image_base": "lost_found",
       "medium": "photography",
       "tags": [
         "found-objects",
         "xxx"
       ],
       "date": "2019",
-      "client": "",
       "recognition": "",
       "images": [
         {
+          "is_cover": true, 
+          "src": "photo1.png",
+          "alt": "alt title"
+        },
+        {
           "src": "",
+          "alt": ""
+        }
+      ]
+    },
+
+    {
+      "name": "3 billion people travel to st. louis, mo, to mourn the loss of 2 victims of capitalism",
+      "image_base": "birds",
+      "medium": "photography",
+      "tags": [
+        "found-objects",
+        "dead-animals",
+        "capitalism"
+      ],
+      "date": "2019",
+      "recognition": "",
+      "images": [
+        {
+          "src": "photo1.png",
+          "alt": "alt title"
+        },
+        {
+          "src": "",
+          "alt": ""
+        }
+      ]
+    },
+
+    {
+      "name": "roadside memorials",
+      "image_base": "roadside_memorials",
+      "medium": "photography",
+      "tags": [
+        "found-objects",
+      ],
+      "date": "2019",
+      "recognition": "",
+      "images": [
+        {
+          "src": "photo1.png",
+          "alt": "alt title"
+        },
+        {
+          "src": "",
+          "alt": ""
+        }
+      ]
+    },
+
+    {
+      "name": "roadkill specimens",
+      "image_base": "roadkill",
+      "medium": "photography",
+      "tags": [
+        "found-objects",
+        "dead-animals"
+      ],
+      "date": "2019",
+      "recognition": "",
+      "images": [
+        {
+          "src": "photo1.png",
           "alt": "alt title"
         },
         {
@@ -20,4 +87,27 @@ let project = [
         }
       ]
     }
+
+    {
+      "name": "unnatural nature",
+      "image_base": "unnatural_nature",
+      "medium": "printmaking",
+      "tags": [
+        "manufactured-wilderness",
+        "embossments"
+      ],
+      "date": "2020",
+      "recognition": "",
+      "images": [
+        {
+          "src": "photo1.png",
+          "alt": "alt title"
+        },
+        {
+          "src": "",
+          "alt": ""
+        }
+      ]
+    }
+
   ]
